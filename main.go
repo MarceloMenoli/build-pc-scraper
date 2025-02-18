af052e6e16a8e4cf4afc82b955497682da8fd918
MarceloMenoli <marcelomenoli7@gmail.com>
2025-02-18 13:17:36 -0300

    feat: call monitor in main file

--- main.go
+++ main.go
@@ -12,11 +12,14 @@ import (
 )
 
 func main() {
-	// Configura e inicia o cron job para atualizar os produtos a cada 30 minutos.
+	// Configura o cron para atualizar os produtos a cada 30 minutos
 	c := cron.New()
 	_, err := c.AddFunc("*/30 * * * *", func() {
 		if err := scraper.UpdateProducts(); err != nil {
 			log.Println("Erro ao atualizar produtos:", err)
+		} else {
+			// Após atualizar, verifica se há algum alerta a ser disparado
+			scraper.VerificaPrecos()
 		}
 	})
 	if err != nil {
@@ -25,14 +28,16 @@ func main() {
 	c.Start()
 	defer c.Stop()
 
-	// Executa uma atualização inicial antes de iniciar o servidor.
+	// Atualização inicial
 	if err := scraper.UpdateProducts(); err != nil {
 		log.Fatal("Erro na atualização inicial:", err)
 	}
+	scraper.VerificaPrecos()
 
-	// Configura o endpoint HTTP.
+	// Configura o endpoint HTTP para expor os produtos
 	http.HandleFunc("/produtos", handlers.ProductsHandler)
 
+	// Use a variável de ambiente PORT se disponível
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
